cd9569693c9f54df072450b7ddd0cdb9bbad9270
Kevin Wu <KWu@lcs.on.ca>
2025-01-13 09:36:12 -0500

    Initial Commit

---
+++ StepperFindSum/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  StepperFindSum
+//
+//  Created by Yixuan Wu on 2025-01-13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ StepperFindSum/StepperFindSumApp.swift
@@ -0,0 +1,17 @@
+//
+//  StepperFindSumApp.swift
+//  StepperFindSum
+//
+//  Created by Yixuan Wu on 2025-01-13.
+//
+
+import SwiftUI
+
+@main
+struct StepperFindSumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
