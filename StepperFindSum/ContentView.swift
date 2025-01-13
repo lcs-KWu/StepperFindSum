2ca2539a224dee5c2f01c124d5a03925498b9e3d
Kevin Wu <KWu@lcs.on.ca>
2025-01-13 15:34:41 -0500

    addition complete

--- StepperFindSum/ContentView.swift
+++ StepperFindSum/ContentView.swift
@@ -8,17 +8,58 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    // MARK: Stored properties
+    @State var firstNumber : Int = 1
+    @State var secondNumber : Int = 1
+    // MARK: Computed properties
+    var Sum : Int {
+        return firstNumber + secondNumber
+    }
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            
+            
+            HStack{
+                Spacer()
+                Text("\(firstNumber)")
+                    .font(.system(size: 96))
+            }
+            HStack{
+                Stepper(value: $firstNumber, label: {
+                    Text("select First Number")})
+                Spacer()
+                
+            }
+            
+            HStack{
+                Text("+")
+                        .font(.system(size: 96))
+                Spacer()
+                Text("\(secondNumber)")
+                    .font(.system(size: 96))
+                
+            }
+            
+            HStack{
+                Stepper(value: $secondNumber, label: {
+                    Text("select Second Number")
+                })}
+            HStack{
+                Spacer()
+                Text("\(Sum)")
+                    .font(.system(size: 96))
+            }
+            .lineLimit(1)
+            .minimumScaleFactor(0.5)
+            
+            
+            
+            .padding()
         }
-        .padding()
     }
 }
-
 #Preview {
     ContentView()
 }
+
